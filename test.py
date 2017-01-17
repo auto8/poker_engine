53fb1d4dd23663580964af98c513f2fa07a1294e
root <root@anon.attlocal.net>
2017-01-17 23:16:40 +0000

    changed some stuff

--- test.py
+++ test.py
@@ -25,18 +25,27 @@ for i in plist:
 	get_rank(x, i)
 
 x = 0
+temp = []
 for i in plist:
 	if i.rank >= x:
 		x = i.rank
 		winner = i
 
+temp.append(winner)
 for i in plist:
-	if i.rank == x:
-		if i.hcard > winner.hcard:
-			display(i.hand)
-			display(winner.hand)
-			display(board)
-			winner = i
-			display(winner.hand)
-		else:
-			continue
+	if i == winner:
+		continue
+	elif i.rank == winner.rank:
+		temp.append(i)
+x = 0 
+if len(temp) > 1:
+	for i in temp:
+		if i.hcard > x:
+			x = i.hcard
+			winner = i 
+	for i in temp:
+		display(i.hand)
+		print(i.hcard)
+
+	display(winner.hand)
+	display(board)	
